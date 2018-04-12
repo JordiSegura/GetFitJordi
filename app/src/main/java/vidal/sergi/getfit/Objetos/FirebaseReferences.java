bc1d185b2066a0f0d83386bd1950d17fd83dc83c
JordiSegura <jordisegura1994@hotmail.com>
2018-04-12 12:27:52 +0200

    Floating Action Button with Animations

---
+++ app/src/main/java/vidal/sergi/getfit/Objetos/FirebaseReferences.java
@@ -0,0 +1,13 @@
+package vidal.sergi.getfit.Objetos;
+
+/**
+ * Created by Sergi on 01/03/2018.
+ */
+
+public class FirebaseReferences {
+    final public static String INGREDIENTES_REFERENCE = "Ingredientes";
+    final public static String GRUPOS_REFERENCE = "Grupos de alimentos";
+
+
+
+}
